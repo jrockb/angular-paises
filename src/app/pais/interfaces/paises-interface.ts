fc6b781378cf4fb24ba20bcad854cd5e22f2e0e4
jrockb87 <jrockb@gmail.com>
2021-06-10 11:07:04 -0500

    llenado de la tabla con la data obtenida y ajustes sobre la pantalla

--- src/app/pais/interfaces/paises-interface.ts
+++ src/app/pais/interfaces/paises-interface.ts
@@ -1,3 +1,4 @@
+/* tslint:disable */
 export interface Pais {
   name:           string;
   topLevelDomain: string[];
--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,4 +1,5 @@
 import { Component} from '@angular/core';
+import { Pais } from '../../interfaces/paises-interface';
 import { PaisService } from '../../services/pais.service';
 
 @Component({
@@ -9,21 +10,26 @@ import { PaisService } from '../../services/pais.service';
 })
 export class PorPaisComponent{
 
-  termino: string = 'Hola Mundo';
+  termino: string = '';
   hayError: boolean = false;
+  paises: Pais[] = [];
 
   constructor(private PaisService: PaisService) { }
 
   buscar(): void{
     this.hayError = false;
-    console.log(this.termino);
+    if (this.termino.length > 0){
+      console.log(this.termino);
     // para que un Observable se dispare se debe tener un subscribe
-    this.PaisService.buscarPais(this.termino)
+      this.PaisService.buscarPais(this.termino)
       .subscribe((paises) => { // primer argumento es la respuesta exitosa
         console.log(paises);
+        this.paises = paises;
       }, (err) => { // segundo argumento para capturar errores
         this.hayError = true;
+        this.paises = [];
       });
+    }
   }
 
 }
